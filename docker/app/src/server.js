66444f0a1bbf4e9272fe43860226f8bf9edb27eb
juanma <jpinero@accedia.es>
2026-06-10 12:00:11 +0200

    changes

--- docker/app/src/server.js
+++ docker/app/src/server.js
@@ -6,25 +6,29 @@ import recetasRouter from "./routes/recetas.route.js";
 
 const app = express();
 
+
 // Para que __filename y __dirname funcionen en ES_MODULES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
+
 // El puerto se coje de la variable de entorno para que sea coherente con el resto del setup
 const PORT = process.env.PORT || 3000;
+
 
 // Configurar el motor de plantillas para que use EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 // Configurar donde guardamos los documentos estáticos
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // ====> RUTAS
 app.get('/', (req,res) => {
     res.render('index',{
-        nombre: "Luis"
+        nombre: "Julian"
     });
 });
 
