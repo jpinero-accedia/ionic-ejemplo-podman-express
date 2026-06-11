c35d6d2adce0b5b5ef7bb8328ff63611ff6c3aca
juanma <jpinero@accedia.es>
2026-06-11 12:05:15 +0200

    acp change

--- docker/app/src/server.js
+++ docker/app/src/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -13,7 +13,8 @@ const __dirname  = path.dirname(__filename);
 
 
 // El puerto se coje de la variable de entorno para que sea coherente con el resto del setup
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 
 // Configurar el motor de plantillas para que use EJS
@@ -26,14 +27,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // ====> RUTAS
+
 app.get('/', (req,res) => {
     res.render('index',{
         nombre: "Julian"
     });
 });
 
+app.use((req,res,next) => {
+    const qry = req.query.q || 'empty';
+    console.log(`QUERY: ${qry}`);
+    if (qry == 'hola' ) {
+        res.send('adios');
+    }
+
+    next();
+} );
+
+app.get('/prueba', (req,res) => {
+    res.send({numeros: [1,2,3,4,5]});
+});
+
+
+app.get('/pjson', (req,res) => {
+    res.json([1,2,3,4,5]);
+});
+
+
 app.use('/receta', recetasRouter);
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Servidor Express corriendo en http://localhost:${PORT}`);
+    console.log(`🚀 Servidor Express corriendo en http://localhost:8000`);
 });
